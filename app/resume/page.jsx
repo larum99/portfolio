23ffcc2f026c5b30a9a5ecb800616999b49479d3
Laura <laura.barona@uao.edu.co>
2026-07-15 12:33:15 -0500

    Updating potfolio

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -28,7 +28,6 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
 
 const education = {
@@ -218,7 +217,6 @@ const resume = () => {
                             <div className="flex flex-col gap-[30px] text-center xl:text-left">
                                 <h3 className="text-4xl font-bold">{education.title}</h3>
                                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{education.description}</p>
-                                <ScrollArea className="h-[400px]">
                                     <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
                                         {education.items.map((item, index) => {
                                             return (
@@ -247,7 +245,6 @@ const resume = () => {
                                             )
                                         })}
                                     </ul>
-                                </ScrollArea>
                             </div>
                         </TabsContent>
                         <TabsContent value="experience" className="w-full">
